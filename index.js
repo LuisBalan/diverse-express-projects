21454ce7d430e0b34b402692f5415b3c35728754
LuisBalan <jlbalcz@gmail.com>
2022-05-24 13:40:29 -0500

    add nodemon index and middleware in index

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+//invoke dependencies and packages
 const express = require('express');
 const mongoose = require('mongoose');
+
+//declare an instance of express
+const app = express();
+
+//middleware that parses incoming requests
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log('Server started at 3000');
+});
