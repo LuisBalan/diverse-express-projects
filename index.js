f4902427fae83fc474eedfc66fef4c63fd8c446d
LuisBalan <jlbalcz@gmail.com>
2022-05-24 14:23:38 -0500

    add script for connection to DB

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 //invoke dependencies and packages
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dontenv').config();
+const dotenv = require('dotenv').config();
 const databaseURL = process.env.DB_URI;
 
-//connection to database
 mongoose.connect(databaseURL);
 const database = mongoose.connection;
+    
+//declaring error to handle if connection fails
+    database.on('error', (error) => {
+        console.log(error);
+    });
+    
+    database.once('connected', () => {
+        console.log('Database connected');
+    });
 
 
 //declare an instance of express
