f2f949977f2e0b5b7aa2603f848741dc01e6f8eb
LuisBalan <jlbalcz@gmail.com>
2022-05-24 14:48:10 -0500

    add routes and endpoints

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,32 @@
 const express = require('express');
-
 const router = express.Router();
+
+// Post Method
+router.post('/post', (req, res) => {
+    res.send('Post API')
+});
+
+//Get all Method
+router.get('/getAll', (req, res) => {
+    res.send('Get All API')
+});
+
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send('Get by ID API')
+});
+
+//Update by ID Method
+router.update('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+});
+
+//Delete by ID Method
+router.delete('/delte/:id', (req, res) => {
+    res.delete('Delete by ID API')
+});
+
+
+
 
 module.exports = router;
