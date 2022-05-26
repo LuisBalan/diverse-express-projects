34343bc3fef9f614fe46349c40fcc8ceef98f540
LuisBalan <jlbalcz@gmail.com>
2022-05-25 22:07:55 -0500

    add POST endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,13 @@ router.post('/post', (req, res) => {
         name: req.body.name,
         age: req.body.age
     });
+
+    try{
+        const dataToSave = data.save();
+        res.status(200).json(dataToSave);
+    }catch(error){
+        res.status(400).json({error: error.message});
+    }
     
     res.send('Post API')
 });
