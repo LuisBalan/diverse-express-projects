ef88c62ff154c1ac40c9a5905553162113732571
LuisBalan <jlbalcz@gmail.com>
2022-06-03 01:13:42 -0500

    add xhr

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-const name = document.getElementsByClassName('name-input');
-const lastName = document.getElementsByClassName('last-name-input');
-const age = document.getElementById('age-input');
+const artistName = document.getElementById('name-input');
+const artistLastName = document.getElementById('last-name-input');
+const artistAge = document.getElementById('age-input');
+
+console.log(artistName.value, artistLastName.value, artistAge.value);
+
+const getNameValue = () => {
+    artistName.addEventListener("change", (event) => {
+        console.log(artistName.value)
+    })
+}
+
+getNameValue()
+
+const postData = () => {
+     const xhr = XMLHttpRequest();
+     xhr.addEventListener("readystatechange", () => {
+         console.log(xhr.readyState)
+     })
+     xhr.onreadystatechange = () => {
+
+     }
+    xhr.open(
+        "POST",
+        "localhost:3000/api/post/",
+        true
+    )
+};
+
+
+
