311d72688040493b40f7f227c34ddfb60af8456f
LuisBalan <jlbalcz@gmail.com>
2022-06-05 22:48:05 -0500

    add postData function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const artistName = document.getElementById('name-input');
 const artistLastName = document.getElementById('last-name-input');
 const artistAge = document.getElementById('age-input');
+const urlPOST = "localhost:3000/api/post/";
 
 console.log(artistName.value, artistLastName.value, artistAge.value);
 
@@ -13,18 +14,25 @@ const getNameValue = () => {
 getNameValue()
 
 const postData = () => {
-     const xhr = XMLHttpRequest();
-     xhr.addEventListener("readystatechange", () => {
-         console.log(xhr.readyState)
-     })
-     xhr.onreadystatechange = () => {
 
-     }
-    xhr.open(
-        "POST",
-        "localhost:3000/api/post/",
-        true
-    )
+    const xhr = XMLHttpRequest();
+    console.log(xhr)
+    xhr.addEventListener("readystatechange", () => {
+         console.log(xhr.readyState)
+         if(xhr.readyState === 4 && xhr.status === 200){
+             console.log(xhr);
+
+             const bodyRequest = {
+                 name: artistName.value,
+                 lastName: artistLastName.value,
+                 age: artistAge.value
+             };
+             xhr.open("POST", urlPOST, true)
+             xhr.send(bodyRequest);
+         }else{
+             console.error('An error ocurried!');
+         }});
+
 };
 
 
