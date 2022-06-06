a886466c8b425ddcede0237cf74c42afc246c8ff
LuisBalan <jlbalcz@gmail.com>
2022-06-05 23:01:10 -0500

    add class tu submit button and handler in js file

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const artistName = document.getElementById('name-input');
 const artistLastName = document.getElementById('last-name-input');
 const artistAge = document.getElementById('age-input');
+const submitButton = document.getElementById('submit-button');
 const urlPOST = "localhost:3000/api/post/";
 
 console.log(artistName.value, artistLastName.value, artistAge.value);
 
-const getNameValue = () => {
-    artistName.addEventListener("change", (event) => {
-        console.log(artistName.value)
-    })
-}
+// const getNameValue = () => {
+//     artistName.addEventListener("change", (event) => {
+//         console.log(artistName.value)
+//     })
+// }
 
-getNameValue()
+// getNameValue()
 
-const postData = () => {
+const postNewItem = () => {
 
     const xhr = XMLHttpRequest();
     console.log(xhr)
+    
     xhr.addEventListener("readystatechange", () => {
          console.log(xhr.readyState)
          if(xhr.readyState === 4 && xhr.status === 200){
@@ -28,12 +30,14 @@ const postData = () => {
                  age: artistAge.value
              };
              xhr.open("POST", urlPOST, true)
-             xhr.send(bodyRequest);
-         }else{
+             xhr.send(bodyRequest);    
+            }else{
              console.error('An error ocurried!');
-         }});
-
+         };
+    });
 };
+
+submitButton.addEventListener("click", postNewItem);
 
 
 
