59677178c6a404b9f49ef7d92da08337f1c52efc
LuisBalan <jlbalcz@gmail.com>
2022-06-06 20:21:25 -0500

    add cors to API

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 //invoke dependencies and packages
 require('dotenv').config();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const databaseURL = process.env.DB_URI;
 const routes = require('./routes/routes');
 const PORT = process.env.PORT || 3000;
-
 //declare an instance of express (starts a new application)
 const app = express();
 
 //middleware that parses incoming requests
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
