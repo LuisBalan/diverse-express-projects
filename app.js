26d7cfcf383b92ed058daf85ab705f906047b6ea
LuisBalan <jlbalcz@gmail.com>
2022-06-06 20:11:00 -0500

    add fetch for request and changes clases of index.html

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
-const artistName = document.getElementById('name-input');
-const artistLastName = document.getElementById('last-name-input');
-const artistAge = document.getElementById('age-input');
-const submitButton = document.getElementById('submit-button');
+//import 'fetch';
+const artistName = document.getElementById("name-input");
+const artistLastName = document.getElementById("last-name-input");
+const artistAge = document.getElementById("age-input");
+const submitButton = document.getElementById("submit-button");
 const urlPOST = "https://myeducationalproject01.uc.r.appspot.com/api/post";
 
-console.log(artistName.value, artistLastName.value, artistAge.value);
 
 // const getNameValue = () => {
-//     artistName.addEventListener("change", (event) => {
-//         console.log(artistName.value)
-//     })
-// }
-
-// getNameValue()
-
-const postNewItem = () => {
-
-    let bodyRequest;
-    const xhr = new XMLHttpRequest();
-    console.log(xhr)
-    xhr.onload = () => {console.log("ready!")}
-    xhr.addEventListener("readystatechange", () => {
-         console.log(xhr.readyState)
-         console.log(xhr.status)
-         if(xhr.readyState === 4 && xhr.status === 200){
-             console.log(xhr);
-
-            bodyRequest = {
-                 name: artistName.value,
-                 lastName: artistLastName.value,
-                 age: artistAge.value
-             };
-             console·log(bodyRequest);
-             
-            }else{
-                console.error('An error ocurried!');
+    //     artistName.addEventListener("change", (event) => {
+        //         console.log(artistName.value)
+        //     })
+        // }
+        
+        // getNameValue()
+        
+        
+    const postNewItem = async (e) => {
+        e.preventDefault();
+        let contentToPost = {
+                name: artistName.value,
+                lastName: artistLastName.value,
+                age: artistAge.value
             };
-        });
-
-        console.log(bodyRequest)
-        xhr.open("POST", urlPOST, true)
-        xhr.send(JSON.stringify(bodyRequest));    
+            
+           await console.log(contentToPost);
+            
+        fetch(urlPOST, {
+            method: "POST",
+            body: JSON.stringify(contentToPost),
+            headers: {"Content-type": "application/json"}
+        })
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err));
 };
 
 submitButton.addEventListener("click", postNewItem);
