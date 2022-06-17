3f4eb3c49548c91b8f3bb5c7b78f7807dcc4c089
LuisBalan <jlbalcz@gmail.com>
2022-06-17 02:49:22 -0500

    modulation of functions

--- js/getMethod.js
+++ js/getMethod.js
@@ -1,24 +1,25 @@
 const urlGetAll = 'https://myeducationalproject01.uc.r.appspot.com/api/getAll';
 const artistsContainer = document.getElementById("legends-cards-container");
 
+// rendering of data
+const displayLegends = (data) => {
+    console.log(data)
+    let legendsContainer = document.createElement("ul");
+    data.map((item) => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        const textNode = document.createTextNode(`Legend: ${item.name} ${item.lastName}, Age: ${item.age}`);
+        a.append(textNode)
+        li.appendChild(a);
+        legendsContainer.appendChild(li);
+    });
+    artistsContainer.appendChild(legendsContainer);
+    //document.body.appendChild(artistsContainer)
+};
+
+//request to DB
 const loadLegends = (e) => {
     e.preventDefault();
-    
-    const displayLegends = (data) => {
-        console.log(data)
-        let legendsContainer = document.createElement("ul");
-        data.map((item) => {
-            const li = document.createElement("li");
-            const a = document.createElement("a");
-            const textNode = document.createTextNode(`Legend: ${item.name} ${item.lastName}, Age: ${item.age}`);
-            a.append(textNode)
-            li.appendChild(a);
-            legendsContainer.appendChild(li);
-        });
-        artistsContainer.appendChild(legendsContainer);
-        //document.body.appendChild(artistsContainer)
-    };
-
         fetch(urlGetAll, {
             method: "GET"
         })
