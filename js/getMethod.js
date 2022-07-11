e43cea2212179365da8145c6e492d83bf7a97130
LuisBalan <jlbalcz@gmail.com>
2022-07-11 02:01:35 -0500

    add legend_datail page and event

--- js/getMethod.js
+++ js/getMethod.js
@@ -1,23 +1,36 @@
 const urlGetAll = 'https://myeducationalproject01.uc.r.appspot.com/api/getAll';
 const artistsContainer = document.getElementById("legends-cards-container");
 
+
+//click action in each legend
+// const getDetailLegend = (e) => {
+//     e.preventDefault();
+//     location.href = './legend_detail.html'
+// }
+
 // rendering of data
 const displayLegends = (data) => {
     console.log(data);
     let legendsContainer = document.createElement("ul");
-    data.map((item) => {
+    legendsContainer.style.listStyle = "none";
+    data.map((item, index) => {
         const li = document.createElement("li");
         const a = document.createElement("a");
-        //const textNode = document.createTextNode(`Legend: ${item.name} ${item.lastName}, Age: ${item.age}`);
+        a.style.cssText = 'color:white';
         a.textContent = `Legend: ${item.name} ${item.lastName}, Age: ${item.age}`;
-        a.href = "https://www.w3schools.com/jsref/prop_anchor_href.asp";
-        //a.append(textNode)
+        a.href = "./legend_detail.html";
+        a.addEventListener('click', e => {
+            e.preventDefault();
+            localStorage.setItem('legendsData', JSON.stringify(data));
+            window.location.href = `./legend_detail.html?legend=${item.name}${item.lastName}`;
+        })
         li.appendChild(a);
         legendsContainer.appendChild(li);
     });
-    artistsContainer.appendChild(legendsContainer);
-    //document.body.appendChild(artistsContainer)
-};
+    artistsContainer.appendChild(legendsContainer); 
+}
+
+console.log(localStorage.length)
 
 //request to DB
 const loadLegends = (e) => {
