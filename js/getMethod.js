b96740e3838383848dcac5982a232479192885fd
LuisBalan <jlbalcz@gmail.com>
2022-07-10 23:57:08 -0500

    set href to each legend anchor

--- js/getMethod.js
+++ js/getMethod.js
@@ -3,13 +3,15 @@ const artistsContainer = document.getElementById("legends-cards-container");
 
 // rendering of data
 const displayLegends = (data) => {
-    console.log(data)
+    console.log(data);
     let legendsContainer = document.createElement("ul");
     data.map((item) => {
         const li = document.createElement("li");
         const a = document.createElement("a");
-        const textNode = document.createTextNode(`Legend: ${item.name} ${item.lastName}, Age: ${item.age}`);
-        a.append(textNode)
+        //const textNode = document.createTextNode(`Legend: ${item.name} ${item.lastName}, Age: ${item.age}`);
+        a.textContent = `Legend: ${item.name} ${item.lastName}, Age: ${item.age}`;
+        a.href = "https://www.w3schools.com/jsref/prop_anchor_href.asp";
+        //a.append(textNode)
         li.appendChild(a);
         legendsContainer.appendChild(li);
     });
