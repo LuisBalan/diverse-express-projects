7723dd26f78c21804842d382491acfefde33bbf3
LuisBalan <jlbalcz@gmail.com>
2022-07-12 15:09:44 -0500

    fix change to another page wen updating item

--- js/legendDetail.js
+++ js/legendDetail.js
@@ -1,17 +1,20 @@
 
+
 const urlGetById = 'https://myeducationalproject01.uc.r.appspot.com/api/getOne';
+const editButton = document.getElementById("edit-button");
+const updateButton = document.getElementById("edit-anchor");
 
 // get from sessionStorage
-console.log(typeof sessionStorage, sessionStorage)
-console.log(typeof sessionStorage, Object.values(sessionStorage))
-console.log(JSON.parse(Object.values(sessionStorage)[0]))
-let legendData = JSON.parse(Object.values(sessionStorage)[0]);
+//console.log(typeof sessionStorage, sessionStorage)
+//console.log(typeof sessionStorage, Object.values(sessionStorage))
+//console.log(JSON.parse(Object.values(sessionStorage)[0]))
+const legendData = JSON.parse(Object.values(sessionStorage)[0]);
 
 //URL building
 console.log(legendData)
 const legendId = legendData._id;
 console.log(legendId);
-console.log(urlGetById+`/${legendId}`)
+console.log(urlGetById +`/${legendId}`);
 
 // rendering legend data on page
 const displayOneLegend = (data) => {
@@ -26,11 +29,11 @@ const displayOneLegend = (data) => {
     // legendContainer.appendChild(nameContainer);
     // legendContainer.appendChild(lastNameContainer);
 
-    let legendContainer = document.getElementsByClassName("legends-cards-container")[0];
+    const legendContainer = document.getElementsByClassName("legends-cards-container")[0];
     const nameContainer = document.createElement("h3");
     const lastNameContainer = document.createElement("h3");
-    let name = document.createTextNode(`${data.name}`);
-    let lastName = document.createTextNode(`${data.lastName}`);
+    const name = document.createTextNode(`${data.name}`);
+    const lastName = document.createTextNode(`${data.lastName}`);
     nameContainer.appendChild(name);
     lastNameContainer.appendChild(lastName);
     legendContainer.appendChild(nameContainer);
@@ -42,21 +45,34 @@ const displayOneLegend = (data) => {
     // let lastName = `${data.lastName}`;
     // nameContainer.textContent = name;
     // lastNameContainer.textContent = lastName;
-
-
 };
-
-
-
 
 const loadOneLegend = (e) => {
     e.preventDefault();
-    fetch(urlGetById+`/${legendId}`, {
+    fetch(urlGetById + `/${legendId}`, {
     method: "GET"
     })
     .then(response => response.json())
     .then(data => displayOneLegend(data))
 };
+
+const changeToUpdatePage = () => {
+    //e.preventDefault();
+    window.location.href = `./update.html?update=${legendId}`;
+}
+
+editButton.addEventListener("click", e => {
+    e.preventDefault();
+    changeToUpdatePage;
+})
+// const updateLegend = (e) => {
+//     e.preventDefault();
+//     fetch(urlGetById + `/${legendId}`, {
+//         method: "PATCH"
+//     })
+//};
+
+
 
 
 window.onload = loadOneLegend;
